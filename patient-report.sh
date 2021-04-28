b9d57d41c0f6866d7e4751716d0b5d851a6054a6
Jan Antala <jan@antala.sk>
2021-04-28 08:36:44 +0200

    Add Monthly Cash Report

--- patient-report.sh
+++ patient-report.sh
@@ -8,4 +8,8 @@ echo "$transformed" | termsql -d ";" "SELECT col3,SUM(col10) As TotalPrice FROM 
 
 echo ""
 echo "Daily Cash Report:"
-echo "$transformed" | termsql -d ";" "SELECT col2,COUNT(col10) As Performances,SUM(col10) As DailyPrice FROM tbl GROUP BY col2"
+echo "$transformed" | termsql -d ";" "SELECT date(col2),COUNT(col10) As Performances,SUM(col10) As DailyPrice FROM tbl GROUP BY col2"
+
+echo ""
+echo "Monthly Cash Report:"
+echo "$transformed" | termsql -d ";" "SELECT date(col2,'start of month'),COUNT(col10) As Performances,SUM(col10) As DailyPrice FROM tbl GROUP BY date(col2,'start of month')"
