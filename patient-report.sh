b2cda12a5cacc0c58e9880d3b8ad5159c957b6f4
Jan Antala <jan@antala.sk>
2021-04-28 08:27:13 +0200

    Add daily cash report

--- patient-report.sh
+++ patient-report.sh
@@ -2,4 +2,10 @@
 
 transformed=`cat "$1" | tail -n +2 | tail -r | tail -n +2 | tail -r`
 
+echo "Patient Cash Report:"
 echo "$transformed" | termsql -d ";" "SELECT col3,SUM(col10) As TotalPrice FROM tbl GROUP BY col3 order by TotalPrice desc"
+
+
+echo ""
+echo "Daily Cash Report:"
+echo "$transformed" | termsql -d ";" "SELECT col2,COUNT(col10) As Performances,SUM(col10) As DailyPrice FROM tbl GROUP BY col2"
